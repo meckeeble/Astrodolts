9d691fb70c98fd9d959ab948dc50603cf1cbfd9a
Max Keeble <meckeeble@hotmail.co.uk>
2020-12-12 12:59:05 +0000

    Add files via upload
    Game server files. Must be launched independently of Unity.

---
+++ GameServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameServer
+{
+    //7:15
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Astrodolts Server";
+            Console.WriteLine("Starting Programme");
+
+            Server.Start(8, 5516);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+	class Server
+	{
+		public static int MaxPlayers { get; private set; }
+		public static int Port { get; private set; }
+		public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+		private static TcpListener tcpListener;
+
+		public static void Start(int _maxPlayers, int _port) {
+			InitialiseServerData();
+
+			MaxPlayers = _maxPlayers;
+			Port = _port;
+
+			tcpListener = new TcpListener(IPAddress.Any, Port);
+			tcpListener.Start();
+			tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback),null);
+
+			Console.WriteLine($"Server started: Port {Port}");
+		}
+
+		public static void TCPConnectCallback(IAsyncResult _result)
+        {
+			TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+			tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+			Console.Write($"Incoming connection from {_client.Client.RemoteEndPoint}");
+			for (int i = 1; i <= MaxPlayers; i++)
+            {
+				if(clients[i].tcp.socket == null)
+                {
+					clients[i].tcp.Connect(_client);
+					return;
+                }
+            }
+
+			Console.WriteLine($"{ _client.Client.RemoteEndPoint}  Connction failed: server full");
+
+        }
+
+		private static void InitialiseServerData()
+        {
+			for (int i = 1; i <= MaxPlayers; i++)
+            {
+				clients.Add(i, new Client(i));
+            }
+        }
+	}
+}
