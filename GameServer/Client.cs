1cdeae0976036f74f26aa156b1679f25929f94d6
Max Keeble <meckeeble@hotmail.co.uk>
2020-12-22 12:47:01 +0000

    Add files via upload
    Server and client now exchanging data via TCP and UDP

--- GameServer/Client.cs
+++ GameServer/Client.cs
@@ -13,11 +13,13 @@ namespace GameServer
 
 		public static int dataBufferSize = 4096;
 		public TCP tcp;
+        public UDP udp;
 
 		public Client(int _clientId)
         {
 			id = _clientId;
 			tcp = new TCP(id);
+            udp = new UDP(id);
         }
 
 		public class TCP
@@ -26,6 +28,7 @@ namespace GameServer
 
 			private readonly int id;
 			private NetworkStream stream;
+			private Packet receivedData;
 			private byte[] receiveBuffer;
 
 			public TCP(int _id)
@@ -41,24 +44,42 @@ namespace GameServer
 
 				stream = socket.GetStream();
 
+				receivedData = new Packet();
 				receiveBuffer = new byte[dataBufferSize];
 
 				stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-
+				ServerSend.Welcome(id, "Welcome to Astrodolts!");
 			}
+
+			public void SendData(Packet _packet)
+            {
+                try
+                {
+					if(socket != null)
+                    {
+						stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
+                    }
+                }
+				catch (Exception _ex)
+                {
+					Console.WriteLine($"Data failed to send to player {id} - Error: {_ex}");
+                }
+            }
 
 			private void ReceiveCallback(IAsyncResult _result)
             {
                 try
                 {
 					int _byteLength = stream.EndRead(_result);
-					if(_byteLength <= 0){
+					if(_byteLength <= 0)
+					{
 						return;
                     }
 
 					byte[] _data = new byte[_byteLength];
 					Array.Copy(receiveBuffer, _data, _byteLength);
 
+                    receivedData.Reset(HandleData(_data));
 					stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
 
                 }
@@ -67,7 +88,90 @@ namespace GameServer
 					Console.WriteLine($"Error receiveing TCP data {_ex}");
                 }
             }
-			
+            private bool HandleData(byte[] _data)
+            {
+                int _packetLength = 0;
+
+                receivedData.SetBytes(_data);
+
+                if (receivedData.UnreadLength() >= 4)
+                {
+                    _packetLength = receivedData.ReadInt();
+                    if (_packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+
+                while (_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
+                {
+                    byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+                    ServerThreadManager.ExecuteOnMainThread(() =>
+                    {
+                        using (Packet _packet = new Packet(_packetBytes))
+                        {
+                            int _packetId = _packet.ReadInt();
+                            Server.packetHandlers[_packetId](id,_packet);
+                        }
+                    });
+
+                    _packetLength = 0;
+                    if (receivedData.UnreadLength() >= 4)
+                    {
+                        _packetLength = receivedData.ReadInt();
+                        if (_packetLength <= 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                if (_packetLength <= 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
-	}
+        
+        public class UDP
+        {
+            public IPEndPoint endPoint;
+            private int id;
+
+            public UDP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(IPEndPoint _endPoint)
+            {
+                Console.WriteLine("Client UDP Connection called");
+                endPoint = _endPoint;
+                ServerSend.UDPTest(id);
+            }
+
+            public void SendData(Packet _packet)
+            {
+                Console.WriteLine("SendData Method of Client object called");
+                Server.SendUDPData(endPoint, _packet);
+            }
+
+            public void HandleData(Packet _packetData)
+            {
+                int _packetLength = _packetData.ReadInt();
+                byte[] _packetBytes = _packetData.ReadBytes(_packetLength);
+
+                ServerThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using (Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        Server.packetHandlers[_packetId](id, _packet);
+                    }
+                        
+                });
+            }
+        }
+    }
 }
--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using System.Threading;
 
 namespace GameServer
 {
-    //7:15
     class Program
     {
+
+        public static bool isRunning = false;
         static void Main(string[] args)
         {
             Console.Title = "Astrodolts Server";
+            isRunning = true;
             Console.WriteLine("Starting Programme");
+
+            Thread mainThread = new Thread(new ThreadStart(MainThread));
+            mainThread.Start();
 
             Server.Start(8, 5516);
 
-            Console.ReadKey();
+        }
+
+        private static void MainThread()
+        {
+            Console.WriteLine($"Main Thread started. Tickrate: {Constants.TICKS_PER_SEC}");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while(_nextLoop < DateTime.Now)
+                {
+                    GameLogic.Update();
+
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    if(_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
         }
     }
 }
